0bab38366478d17db2fabd54370d77b11acac199
Cassiano Menezes <cassiano_menezes@terceiros.sicredi.com.br>
2018-04-05 16:07:50 -0300

    Beer call

--- app/src/main/java/com/cassianomenezes/mytransferwise/network/RetrofitModel.java
+++ app/src/main/java/com/cassianomenezes/mytransferwise/network/RetrofitModel.java
@@ -1,6 +1,9 @@
 package com.cassianomenezes.mytransferwise.network;
 
+import com.cassianomenezes.mytransferwise.entries.Beer;
 import com.cassianomenezes.mytransferwise.entries.FootballResponse;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -9,5 +12,8 @@ public interface RetrofitModel {
 
     @GET("v1/teams/5/players")
     Call<FootballResponse> getPlayersInfo();
+
+    @GET("v2/beers")
+    Call<List<Beer>> getBeerInfo();
 
 }
